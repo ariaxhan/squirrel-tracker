e1c570e99b1afa50f25f23f79c36f98dbf98261a
Aria Han <ariaxhan@gmail.com>
2024-08-03 16:09:40 -0700

    squirrels are cute

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        <h1>Police Tracker</h1>
+        <p>We see you too.</p>
+        <button>Upload</button>
+      </div>
+      <div className="police"></div>
+      <div className="ant"></div>
+      <div className="gun-image gun-1"></div>
+      <div className="gun-image gun-2"></div>
+      <div className="gun-image gun-3"></div>
+      <div className="blob blob-1"></div>
+      <div className="blob blob-2"></div>
+      <div className="blob blob-3"></div>
+      <div className="blob blob-4"></div>
+      <div className="blob blob-5"></div>
     </div>
   );
 }
